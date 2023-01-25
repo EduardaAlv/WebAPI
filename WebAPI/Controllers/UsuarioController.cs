1bc18c800b4ef2bbaba6e0f2f16fde07157c90cd
EduardaAlv <eduardasantosalveseduarda@gmail.com>
2023-01-24 21:32:46 -0300

    Criação da Controller UsuarioController

---
+++ WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortDateString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("ObterDataHoraAtual/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo";
+            return Ok(new { mensagem });
+        }
+
+    }
+}
